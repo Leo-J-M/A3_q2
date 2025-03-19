c8ecb303567a09df2526403d422d0127f886bfd0
Lionel JM <97952481+Lionel-JM@users.noreply.github.com>
2025-03-19 19:59:57 +0000

    Added functionality

--- A3_q2/Form1.Designer.cs
+++ A3_q2/Form1.Designer.cs
@@ -26,8 +26,7 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             label1 = new Label();
             txtGues = new TextBox();
             btnNewGame = new Button();
@@ -41,73 +40,89 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(33, 92);
+            label1.Font = new Font("Segoe UI", 11F);
+            label1.Location = new Point(67, 171);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(141, 15);
+            label1.Size = new Size(265, 30);
             label1.TabIndex = 0;
             label1.Text = "Enter your gues (0-1000): ";
             // 
             // txtGues
             // 
-            txtGues.Location = new Point(180, 89);
+            txtGues.Font = new Font("Segoe UI", 11F);
+            txtGues.Location = new Point(337, 168);
+            txtGues.Margin = new Padding(4, 5, 4, 5);
             txtGues.Name = "txtGues";
-            txtGues.Size = new Size(100, 23);
+            txtGues.Size = new Size(141, 37);
             txtGues.TabIndex = 1;
-            txtGues.TextAlign = HorizontalAlignment.Right;
+            txtGues.TextAlign = HorizontalAlignment.Center;
             // 
             // btnNewGame
             // 
-            btnNewGame.Location = new Point(205, 23);
+            btnNewGame.Font = new Font("Segoe UI", 11F);
+            btnNewGame.Location = new Point(326, 56);
+            btnNewGame.Margin = new Padding(4, 5, 4, 5);
             btnNewGame.Name = "btnNewGame";
-            btnNewGame.Size = new Size(75, 23);
+            btnNewGame.Size = new Size(152, 38);
             btnNewGame.TabIndex = 2;
-            btnNewGame.Text = "New Game";
+            btnNewGame.Text = "N&ew Game";
             btnNewGame.UseVisualStyleBackColor = true;
             btnNewGame.Click += btnNewGame_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(33, 27);
+            label2.Font = new Font("Segoe UI", 11F);
+            label2.Location = new Point(67, 63);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(69, 15);
+            label2.Size = new Size(126, 30);
             label2.TabIndex = 3;
             label2.Text = "Games won";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(33, 61);
+            label3.Font = new Font("Segoe UI", 11F);
+            label3.Location = new Point(67, 120);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(72, 15);
+            label3.Size = new Size(131, 30);
             label3.TabIndex = 4;
             label3.Text = "Atempts left";
             // 
             // txtWon
             // 
-            txtWon.Location = new Point(108, 24);
+            txtWon.Font = new Font("Segoe UI", 11F);
+            txtWon.Location = new Point(206, 57);
+            txtWon.Margin = new Padding(4, 5, 4, 5);
             txtWon.Name = "txtWon";
             txtWon.ReadOnly = true;
-            txtWon.Size = new Size(66, 23);
+            txtWon.Size = new Size(93, 37);
             txtWon.TabIndex = 5;
             txtWon.Text = "0";
             txtWon.TextAlign = HorizontalAlignment.Center;
             // 
             // txtAtempts
             // 
-            txtAtempts.Location = new Point(108, 58);
+            txtAtempts.Font = new Font("Segoe UI", 11F);
+            txtAtempts.Location = new Point(206, 117);
+            txtAtempts.Margin = new Padding(4, 5, 4, 5);
             txtAtempts.Name = "txtAtempts";
             txtAtempts.ReadOnly = true;
-            txtAtempts.Size = new Size(66, 23);
+            txtAtempts.Size = new Size(93, 37);
             txtAtempts.TabIndex = 6;
             txtAtempts.Text = "10";
-            txtAtempts.TextAlign = HorizontalAlignment.Right;
+            txtAtempts.TextAlign = HorizontalAlignment.Center;
             // 
             // btnGues
             // 
-            btnGues.Location = new Point(33, 129);
+            btnGues.Font = new Font("Segoe UI", 11F);
+            btnGues.Location = new Point(67, 233);
+            btnGues.Margin = new Padding(4, 5, 4, 5);
             btnGues.Name = "btnGues";
-            btnGues.Size = new Size(247, 23);
+            btnGues.Size = new Size(411, 38);
             btnGues.TabIndex = 7;
             btnGues.Text = "Gues";
             btnGues.UseVisualStyleBackColor = true;
@@ -115,9 +130,9 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(304, 224);
+            ClientSize = new Size(565, 365);
             Controls.Add(btnGues);
             Controls.Add(txtAtempts);
             Controls.Add(txtWon);
@@ -126,8 +141,10 @@
             Controls.Add(btnNewGame);
             Controls.Add(txtGues);
             Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Gues Number";
             ResumeLayout(false);
             PerformLayout();
         }
--- A3_q2/Form1.cs
+++ A3_q2/Form1.cs
@@ -1,45 +1,91 @@
 using System;
+using Microsoft.VisualBasic.ApplicationServices;
 
 namespace A3_q2
 {
-    public partial class Form1 : Form
-    {
+    public partial class Form1 : Form {
         int randomNum;
         int atemptsLeft;
         int gamesWon;
-        
+
 
         Random random = new Random();
 
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
-            
+            this.AcceptButton = btnGues;
+
             randomNum = random.Next(1, 1000);
             atemptsLeft = 10;
             gamesWon = 0;
         }
 
-        private void btnGues_Click(object sender, EventArgs e)
-        {
+        private void btnGues_Click(object sender, EventArgs e) {
+            if (!txtGues.Text.All(char.IsDigit)) {
+                MessageBox.Show("Invalid input! Enter 1-1000.");
+                txtGues.Text = "";
+                return;
+            }
+            
+            int userGues = Convert.ToInt32(txtGues.Text);//
+
+            if (userGues > 1000 || userGues < 1) {
+                MessageBox.Show("Invalid input! Enter 1-1000.");
+                txtGues.Text = "";
+                return;
+            }
+
+            if (randomNum == userGues) {
+                if (atemptsLeft == 1) {
+                    MessageBox.Show("Either you know the secret or you got lucky!");
+                }
+                else {
+                    MessageBox.Show("You know the secret!");
+                }
+                txtWon.Text = (++gamesWon).ToString();
+                DialogResult result = MessageBox.Show("Start new game?",
+                                                          "Click Yes",
+                                                          MessageBoxButtons.YesNo,
+                                                          MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes) {
+                    btnNewGame_Click(sender, e);
+                }
+                return;
+            }
+
+            else if (atemptsLeft <= 1) {
+
+                btnGues.Enabled = false;
+                DialogResult result = MessageBox.Show("You should be able to do better! Try again?",
+                                                          "New game?",
+                                                          MessageBoxButtons.YesNo,
+                                                          MessageBoxIcon.Information);
+
+                if (result == DialogResult.Yes) {
+                    btnNewGame_Click(sender, e);
+                    return;
+                }
+            }
+
+            else if (randomNum > userGues) {
+                MessageBox.Show("Too Low, Try again.");
+            }
+
+            else if (randomNum < userGues) {
+                MessageBox.Show("Too HIGH, Try again.");
+            }
+
+            txtAtempts.Text = (--atemptsLeft).ToString();
+            txtGues.Text = "";
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e) {
+            btnGues.Enabled = true;
             randomNum = random.Next(1, 1000);
             atemptsLeft = 10;
             txtAtempts.Text = atemptsLeft.ToString();
             txtGues.Text = "";
-        }
-
-        private void btnNewGame_Click(object sender, EventArgs e)
-        {
-            int userGues = Convert.ToInt32(txtGues.Text);//
-            if (randomNum == userGues && atemptsLeft == 1) {
-
-            }
-            else if (randomNum == userGues && atemptsLeft > 1) { 
-                
-            }
-            else if(randomNum != userGues) {
-
-            }
         }
     }
 }
